89055184589d007d134f4d217dfc769a2f46b4ce
Abdullah Eldahrawy <abdullah.sherif2001@gmail.com>
2025-08-27 00:21:11 +0300

    added invitedby column to rsvp model

--- src/rsvpM.js
+++ src/rsvpM.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const rsvpSchema = new mongoose.Schema({
   name: { type: String, required: true },
   phone: { type: String, required: true, unique: true },
-  coming: { type: Boolean, required: true }
+  coming: { type: Boolean, required: true },
+  initedBy: { type: String, default: "Abdullah"}
 });
 
 module.exports = mongoose.model('Rsvp', rsvpSchema);
